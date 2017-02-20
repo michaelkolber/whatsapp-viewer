4a2ca69a7b9d6c94f09d9883c86501a0b9306b10
Michael Kolber <michaelkolber@users.noreply.github.com>
2017-02-20 19:01:49 +0200

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,25 +1,23 @@
 $(function() {
     $('button').click(submitForm);
-    $("input").keypress(function(event) {
+    /*$("input").keypress(function(event) {
         if (event.which == 13) {
             event.preventDefault();
             submitForm();
         }
-    });
+    });*/
 });
 
 function submitForm() {
-        //var file = $('input').val();
-        //$('button').html("Uploading...")
-        //var files = $('#file-select').files;
-        var formData = new FormData();
+        var chatTranscript = $('#fileSelector');
+        var file = new FormData(chatTranscript);
         $.ajax({
             url: 'https://meb6vjwdn2.execute-api.us-east-1.amazonaws.com/newstage/WhatsAppFileManager',
-            data: '"' + $('#namebox').val() + '"',
+            data: file,
             type: 'POST',
             success: function(response) {
-                $('#chatname').html(response);
-                console.log("Success; the chat name has been set to '" + response + "'");
+                $('#chatbox').html(response);
+                console.log("Success");
             },
             error: function(error) {
                 console.log("Error:" + error);
