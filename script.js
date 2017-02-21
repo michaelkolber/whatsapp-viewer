eeddc6aeceaac7d6a6dccae8be21a98a9514df2d
Michael Kolber <michaelkolber@users.noreply.github.com>
2017-02-21 17:21:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,22 @@ $('#switchButton').click(function() {
         switched = 0;
     }
 });
+    
+$('#uploadNew').click(openUploadBox);
+$('#closeuploadbox').click(closeUploadBox);
 
 
-$('button').click(submitForm);
+$('#uploadbutton').click(function() {
+    if(!$("#fileSelector")[0].files[0]) {}
+    else {uploadFile();}
+});
+    
+$('#samplebutton').click(loadSampleFile);
 
 });
 
-function submitForm() {
+function uploadFile() {
+    $('#uploadbutton').html('<i class="fa fa-refresh fa-spin fa-fw"></i> Uploading...');
     var file = $('#fileSelector')[0].files[0];
     var reader = new FileReader();
     reader.readAsText(file);
@@ -42,10 +51,45 @@ function submitForm() {
                 names = response["names"];
                 $('#chatname').html("Chat with " + names[0]);
                 $('#chatbox').html(response["chat"]);
+                closeUploadBox();
+                $('#samplebutton').html('<i class="fa fa-file-text" aria-hidden="true"></i> Load Sample File');
             },
             error: function(error) {
                 console.log("Error:", error);
+                $('#uploadbutton').html('<i class="fa fa-upload" aria-hidden="true"></i> Upload');
             }
         });
     });
+}
+
+function loadSampleFile() {
+    $('#samplebutton').html('<i class="fa fa-refresh fa-spin fa-fw"></i> Loading...');
+    $.ajax({
+        url: 'https://meb6vjwdn2.execute-api.us-east-1.amazonaws.com/newstage/WhatsAppFileManager',
+        data: "loadsamplefile",
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            names = response["names"];
+            $('#chatname').html("Chat with " + names[0]);
+            $('#chatbox').html(response["chat"]);
+            closeUploadBox();
+            $('#samplebutton').html('<i class="fa fa-file-text" aria-hidden="true"></i> Load Sample File');
+        },
+        error: function(error) {
+            console.log("Error:", error);
+            $('#samplebutton').html('<i class="fa fa-file-text" aria-hidden="true"></i> Load Sample File');
+        }
+    });
+}
+
+function closeUploadBox() {
+    $('#shade').css({"display": "none"});
+    $('#uploadboxcontainer').css({"display": "none"});
+}
+
+function openUploadBox() {
+    $('#shade').css({"display": ""});
+    $('#uploadboxcontainer').css({"display": ""});
 }
